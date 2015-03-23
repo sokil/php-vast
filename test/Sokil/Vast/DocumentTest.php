7ffeeb59357ca0fc30ac8e28a3e6d08172c31625
De Sokil <mayhem@ukr.net>
2015-03-23 12:25:03 +0200

    Update DocumentTest.php

--- test/Sokil/Vast/DocumentTest.php
+++ test/Sokil/Vast/DocumentTest.php
@@ -4,5 +4,9 @@ namespace Sokil\Vast;
 
 class DocumentTest extends \PHPUnit_Framework_TestCase
 {
-
+    public function testCreate()
+    {
+        $document = \Sokil\Vast\Document::create('2.0');
+        $this->assertInstanceOf('\Sokil\Vast\Document', $document);
+    }
 }
