9c843a915a452925c2eb5a06b57c56f6dbb4e8d6
Dmytro Sokil <sokol@auroraglobal.com>
2015-07-10 15:57:33 +0300

    php 5.3 compatimble

--- tests/Sokil/Vast/DocumentTest.php
+++ tests/Sokil/Vast/DocumentTest.php
@@ -31,7 +31,7 @@ class DocumentTest extends \PHPUnit_Framework_TestCase
                 ->setWidth(100)
                 ->setUrl('http://server.com/media.mp4');
 
-        $actualXml = str_replace(["\r", "\n"], '', $document->toString());
+        $actualXml = str_replace(array("\r", "\n"), '', $document->toString());
 
         $expectedXml = '<?xml version="1.0" encoding="UTF-8"?><VAST version="2.0"><Ad id="ad1"><InLine><AdSystem>Ad Server Name</AdSystem><AdTitle><![CDATA[Ad Title]]></AdTitle><Impression><![CDATA[http://ad.server.com/impression]]></Impression><Creatives><Creative><Linear><Duration>00:02:08</Duration><VideoClicks><ClickThrough><![CDATA[http://entertainmentserver.com/landing]]></ClickThrough><ClickTracking><![CDATA[http://ad.server.com/videoclicks/clicktracking]]></ClickTracking><CustomClick><![CDATA[http://ad.server.com/videoclicks/customclick]]></CustomClick></VideoClicks><TrackingEvents><Tracking event="start"><![CDATA[http://ad.server.com/trackingevent/start]]></Tracking><Tracking event="pause"><![CDATA[http://ad.server.com/trackingevent/stop]]></Tracking></TrackingEvents><MediaFiles><MediaFile delivery="progressive" type="video/mp4" height="100" width="100"><![CDATA[http://server.com/media.mp4]]></MediaFile></MediaFiles></Linear></Creative></Creatives></InLine></Ad></VAST>';
 
