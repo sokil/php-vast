3bbb28a2767596f5e28ecbd85b303746fe6b5523
Toni Gurski <tg@mandarin-medien.de>
2016-03-23 20:21:01 +0100

    Wrapper-Node can now hold an AdSystem-Node

--- src/Sokil/Vast/Ad/Wrapper.php
+++ src/Sokil/Vast/Ad/Wrapper.php
@@ -4,6 +4,13 @@ namespace Sokil\Vast\Ad;
 
 class Wrapper extends \Sokil\Vast\Ad
 {
+
+    /**
+     * adds a VASTAdTagURI-Node to Wrapper-Node
+     *
+     * @param $uri
+     * @return $this
+     */
     public function addVASTAdTagURI($uri)
     {
         // create VASTAdTagURI-Node
@@ -13,7 +20,27 @@ class Wrapper extends \Sokil\Vast\Ad
         // create VASTAdTagURI-cdata
         $cdata = $this->_domElement->ownerDocument->createCDATASection($uri);
         $VASTAdTagURIDomElement->appendChild($cdata);
-        
+
+        return $this;
+    }
+
+    /**
+     * adds a AdSystem-Node to Wrapper-Node
+     *
+     * @param string $adSystem
+     * @return \Sokil\Vast\Ad\Wrapper
+     */
+    public function setAdSystem($adSystem)
+    {
+        $adSystemDomElement = $this->_domElement->getElementsByTagName('AdSystem')->item(0);
+        if($adSystemDomElement) {
+            $adSystemDomElement->nodeValue = $adSystem;
+        }
+        else {
+            $adSystemDomElement = $this->_domElement->ownerDocument->createElement('AdSystem', $adSystem);
+            $this->_domElement->firstChild->appendChild($adSystemDomElement);
+        }
+
         return $this;
     }
 }
