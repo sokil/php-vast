a7af557ab12d852ede6abd606f43b42f16c8f8e4
Toni Gurski <tg@mandarin-medien.de>
2016-03-23 20:03:17 +0100

    Wrapper-Node can now hold a VASTAdTagURI-Node

--- src/Sokil/Vast/Ad/Wrapper.php
+++ src/Sokil/Vast/Ad/Wrapper.php
@@ -4,5 +4,16 @@ namespace Sokil\Vast\Ad;
 
 class Wrapper extends \Sokil\Vast\Ad
 {
-    
+    public function addVASTAdTagURI($uri)
+    {
+        // create VASTAdTagURI-Node
+        $VASTAdTagURIDomElement = $this->_domElement->ownerDocument->createElement('VASTAdTagURI');
+        $this->_domElement->firstChild->appendChild($VASTAdTagURIDomElement);
+
+        // create VASTAdTagURI-cdata
+        $cdata = $this->_domElement->ownerDocument->createCDATASection($uri);
+        $VASTAdTagURIDomElement->appendChild($cdata);
+        
+        return $this;
+    }
 }
