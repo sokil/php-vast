7b4941c7965343fbaef11bbe38b8435d1a99fc2a
Anton Kolenkov <dorantor@gmail.com>
2017-07-18 18:49:13 +0300

    Code formatting. Phpdocs.

--- src/Sokil/Vast/Ad/InLine.php
+++ src/Sokil/Vast/Ad/InLine.php
@@ -2,28 +2,30 @@
 
 namespace Sokil\Vast\Ad;
 
-/**
- * @method \Sokil\Vast\Ad\InLine setId(integer $id) Set "id" of Ad section
- */
 class InLine extends \Sokil\Vast\Ad
 {
+    /**
+     * Creatives
+     *
+     * @var array
+     */
     private $creatives = array();
     
     /**
-     *
      * @var \DomElement
      */
     private $creativesDomElement;
     
     /**
-     *
      * @var \DomElement
      */
     private $extensionsDomElement;
     
     /**
-     * 
+     * Set Ad title
+     *
      * @param string $adTitle
+     *
      * @return \Sokil\Vast\Ad\InLine
      */
     public function setAdTitle($adTitle)
@@ -32,18 +34,15 @@ class InLine extends \Sokil\Vast\Ad
     
         // get AdTitle tag
         $adTitleDomElement = $this->domElement->getElementsByTagName('AdTitle')->item(0);
-        if(!$adTitleDomElement) {
+        if (!$adTitleDomElement) {
             $adTitleDomElement = $this->domElement->ownerDocument->createElement('AdTitle', $adTitle);
             $this->domElement->firstChild->appendChild($adTitleDomElement);
         }
         
         // update CData
-        if($adTitleDomElement->hasChildNodes()) {
+        if ($adTitleDomElement->hasChildNodes()) {
             $adTitleDomElement->replaceChild($cdata, $adTitleDomElement->firstChild);
-        }
-        
-        // insert cdata
-        else {
+        } else {  // insert cdata
             $adTitleDomElement->appendChild($cdata);
         }
         
@@ -51,7 +50,8 @@ class InLine extends \Sokil\Vast\Ad
     }
     
     /**
-     * 
+     * Set impression tracking url
+     *
      * @param string $url
      * @return \Sokil\Vast\Ad\InLine
      */
@@ -61,18 +61,15 @@ class InLine extends \Sokil\Vast\Ad
         
         // get impression tag
         $impressionDomElement = $this->domElement->getElementsByTagName('Impression')->item(0);
-        if(!$impressionDomElement) {
+        if (!$impressionDomElement) {
             $impressionDomElement = $this->domElement->ownerDocument->createElement('Impression');
             $this->domElement->firstChild->appendChild($impressionDomElement);
         }
         
         // update CData
-        if($impressionDomElement->hasChildNodes()) {
+        if ($impressionDomElement->hasChildNodes()) {
             $impressionDomElement->replaceChild($cdata, $impressionDomElement->firstChild);
-        }
-        
-        // insert cdata
-        else {
+        } else { // insert cdata
             $impressionDomElement->appendChild($cdata);
         }
         
@@ -80,22 +77,26 @@ class InLine extends \Sokil\Vast\Ad
     }
     
     /**
-     * 
-     * @return \Sokil\Vast\Ad\InLine\Creative
+     * Create "creative" object of given type
+     *
+     * @param string $type
+     * @throws \Exception
+     *
+     * @return \Sokil\Vast\Ad\InLine\Creative\Base
      */
     private function _createCreative($type)
     {
         // check type
         $creativeClassName = '\\Sokil\Vast\\Ad\\InLine\\Creative\\' . $type;
-        if(!class_exists($creativeClassName)) {
+        if (!class_exists($creativeClassName)) {
             throw new \Exception('Wrong crative specified');
         }
         
         // get container
-        if(!$this->creativesDomElement) {
+        if (!$this->creativesDomElement) {
             // get creatives tag
             $this->creativesDomElement = $this->domElement->getElementsByTagName('Creatives')->item(0);
-            if(!$this->creativesDomElement) {
+            if (!$this->creativesDomElement) {
                 $this->creativesDomElement = $this->domElement->ownerDocument->createElement('Creatives');
                 $this->domElement->firstChild->appendChild($this->creativesDomElement);
             }
@@ -124,14 +125,22 @@ class InLine extends \Sokil\Vast\Ad
     {
         return $this->_createCreative('Linear');
     }
-    
+
+    /**
+     * Add extension
+     *
+     * @param string $type
+     * @param string $value
+     *
+     * @return $this
+     */
     public function addExtension($type, $value)
     {
         // get container
-        if(!$this->extensionsDomElement) {
+        if (!$this->extensionsDomElement) {
             // get creatives tag
             $this->extensionsDomElement = $this->domElement->getElementsByTagName('Extensions')->item(0);
-            if(!$this->extensionsDomElement) {
+            if (!$this->extensionsDomElement) {
                 $this->extensionsDomElement = $this->domElement->ownerDocument->createElement('Extensions');
                 $this->domElement->firstChild->appendChild($this->extensionsDomElement);
             }
