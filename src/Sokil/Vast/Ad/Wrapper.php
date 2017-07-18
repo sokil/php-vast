386647d327562efa1b8231aaa49f40449c8da2b8
Anton Kolenkov <dorantor@gmail.com>
2017-07-18 19:52:00 +0300

    Reused UniqTag trait logic in Wrapper class.

--- src/Sokil/Vast/Ad/Wrapper.php
+++ src/Sokil/Vast/Ad/Wrapper.php
@@ -24,24 +24,6 @@ class Wrapper extends \Sokil\Vast\Ad
      */
     public function setVASTAdTagURI($uri)
     {
-        $cdata = $this->domElement->ownerDocument->createCDATASection($uri);
-
-        // get VASTAdTagURI dom node
-        $VASTAdTagURIDomElement = $this->domElement->getElementsByTagName('VASTAdTagURI')->item(0);
-
-        // create VASTAdTagURI-Node
-        if (!$VASTAdTagURIDomElement) {
-            $VASTAdTagURIDomElement = $this->domElement->ownerDocument->createElement('VASTAdTagURI');
-            $this->domElement->firstChild->appendChild($VASTAdTagURIDomElement);
-        }
-
-        // set cdata
-        if ($VASTAdTagURIDomElement->hasChildNodes()) {
-            $VASTAdTagURIDomElement->replaceChild($cdata, $VASTAdTagURIDomElement->firstChild);
-        } else {
-            $VASTAdTagURIDomElement->appendChild($cdata);
-        }
-
-        return $this;
+        return $this->setTagValue('VASTAdTagURI', $uri);
     }
 }
