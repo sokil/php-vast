164f1d1f5d91bf432cd12e8dcee1fd7fed5f019a
Anton Kolenkov <dorantor@gmail.com>
2018-10-30 23:45:32 +0300

    Added test mediaFile url update.

--- tests/DocumentTest.php
+++ tests/DocumentTest.php
@@ -45,6 +45,46 @@ class DocumentTest extends AbstractTestCase
     }
 
     /**
+     * test for mediaFile url update
+     */
+    public function testMediaFileUrlUpdate()
+    {
+        $factory = new Factory();
+        $document = $factory->create('2.0');
+
+        // insert Ad section
+        $ad1 = $document
+            ->createInLineAdSection()
+            ->setId('ad1')
+            ->setAdSystem('Ad Server Name')
+            ->setAdTitle('Ad Title')
+            ->addImpression('http://ad.server.com/impression', 'imp1');
+
+        // create creative for ad section
+        /** @var \Sokil\Vast\Creative\InLine\Linear\MediaFile $mediaFile */
+        $mediaFile = $ad1
+            ->createLinearCreative()
+            ->setDuration(128)
+            ->setVideoClicksClickThrough('wrongLink')
+            ->setVideoClicksClickThrough('http://entertainmentserver.com/landing')
+            ->addVideoClicksClickTracking('http://ad.server.com/videoclicks/clicktracking')
+            ->addVideoClicksCustomClick('http://ad.server.com/videoclicks/customclick')
+            ->addTrackingEvent('start', 'http://ad.server.com/trackingevent/start')
+            ->addTrackingEvent('pause', 'http://ad.server.com/trackingevent/stop')
+            ->createMediaFile()
+                ->setProgressiveDelivery()
+                ->setType('video/mp4')
+                ->setHeight(100)
+                ->setWidth(100)
+                ->setBitrate(600)
+                ->setUrl('wrong url')
+        ;
+        // updating to correct url
+        $mediaFile->setUrl('http://server.com/media.mp4');
+        $this->assertFileVsDocument('inlineAd.xml', $document);
+    }
+
+    /**
      * Test for inline ad
      */
     public function testGetAdSection()
